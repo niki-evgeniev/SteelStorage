eb6fdf47cf09dfc54cf26339756b7d81da0fcdc5
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-03-27 00:14:15 +0200

    new spring project

---
+++ src/main/java/nevg/steelstorage/SteelStorageApplication.java
@@ -0,0 +1,13 @@
+package nevg.steelstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteelStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteelStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nevg/steelstorage/SteelStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package nevg.steelstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteelStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
