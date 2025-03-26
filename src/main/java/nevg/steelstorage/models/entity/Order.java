a56336c48c8ff7d3dbb98a39c9f47a392f277f8c
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-03-27 00:56:32 +0200

    Add table Order

---
+++ src/main/java/nevg/steelstorage/models/entity/Order.java
@@ -0,0 +1,44 @@
+package nevg.steelstorage.models.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Table
+@Entity(name = "orders")
+public class Order extends BaseEntity {
+
+    @Column(name = "added_date")
+    private LocalDateTime addedDate;
+
+    @Column(name = "added_quantity")
+    private int addedQuantity;
+
+    @Column(name = "worked_quantity")
+    private int workedQuantity;
+
+    @ManyToOne
+    private User orders;
+
+    public Order() {
+    }
+
+    public LocalDateTime getAddedDate() {
+        return addedDate;
+    }
+
+    public void setAddedDate(LocalDateTime addedDate) {
+        this.addedDate = addedDate;
+    }
+
+    public User getOrders() {
+        return orders;
+    }
+
+    public void setOrders(User orders) {
+        this.orders = orders;
+    }
+}
