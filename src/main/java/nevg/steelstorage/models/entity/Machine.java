e5d919d32b59f79db38bc32ee9292495c79df67d
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-03-27 00:56:22 +0200

    Add table Machine

---
+++ src/main/java/nevg/steelstorage/models/entity/Machine.java
@@ -0,0 +1,49 @@
+package nevg.steelstorage.models.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.util.List;
+
+@Table
+@Entity(name = "machines")
+public class Machine extends BaseEntity {
+
+    @Column(name = "machine_name")
+    private String machineName;
+
+    @Column(name = "serial_number", unique = true)
+    private String serialNumber;
+
+    @ManyToOne
+    private User machineList;
+
+    public Machine() {
+    }
+
+    public String getMachineName() {
+        return machineName;
+    }
+
+    public void setMachineName(String machineName) {
+        this.machineName = machineName;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public User getMachineList() {
+        return machineList;
+    }
+
+    public void setMachineList(User machineList) {
+        this.machineList = machineList;
+    }
+}
