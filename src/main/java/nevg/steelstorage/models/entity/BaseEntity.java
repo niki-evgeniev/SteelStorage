f0d60e3a134c2b3d62975978950f17e7db77c536
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-03-27 00:56:10 +0200

    Add BaseEntity

---
+++ src/main/java/nevg/steelstorage/models/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package nevg.steelstorage.models.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+import java.util.UUID;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uuid;
+
+    public BaseEntity() {
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+}
