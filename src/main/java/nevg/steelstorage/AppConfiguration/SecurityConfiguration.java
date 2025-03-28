3f1dd25b4a87eb6a3b27fd2fe36c07899e2c6b91
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-03-29 01:46:22 +0200

    Add Spring security configuration

---
+++ src/main/java/nevg/steelstorage/AppConfiguration/SecurityConfiguration.java
@@ -0,0 +1,75 @@
+package nevg.steelstorage.AppConfiguration;
+
+
+import jakarta.servlet.http.HttpServletResponse;
+import nevg.steelstorage.Repository.UserRepository;
+import nevg.steelstorage.Service.Impl.SteelStorageUserService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    private final String rememberMeKey;
+
+    public SecurityConfiguration(@Value("${carpmap.remember.me.key}")
+                                 String rememberMeKey) {
+        this.rememberMeKey = rememberMeKey;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authorizeHttpRequests(
+                authorizeRequest -> authorizeRequest
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/js/**", "/fonts/**", "/css/**", "/img/**").permitAll()
+                        .requestMatchers("/", "/sign_up", "/sign_in").permitAll()
+                        .anyRequest().authenticated()
+
+        ).formLogin(
+                formLogin -> {
+                    formLogin
+                            .loginPage("/sign_in")
+                            .usernameParameter("email")
+                            .passwordParameter("password")
+                            .defaultSuccessUrl("/", true)
+                            .failureForwardUrl("/users/login-error");
+                }
+        ).logout(
+                logout -> {
+                    logout.logoutUrl("/users/logout")
+                            .logoutSuccessUrl("/")
+                            .invalidateHttpSession(true);
+
+                }
+        ).rememberMe(
+                rememberMe -> {
+                    rememberMe
+                            .key(rememberMeKey)
+                            .rememberMeParameter("remember-me")
+                            .rememberMeCookieName("remember-me");
+                }
+        );
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return new SteelStorageUserService(userRepository);
+    }
+}
