5bb2cf1ec321055a208d6ed7720ec14616640789
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-03-29 02:40:30 +0200

    update

--- src/main/java/nevg/steelstorage/Controller/UserController.java
+++ src/main/java/nevg/steelstorage/Controller/UserController.java
@@ -1,7 +1,12 @@
 package nevg.steelstorage.Controller;
 
+import jakarta.validation.Valid;
+import nevg.steelstorage.Models.DTO.RegisterNewUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -15,5 +20,24 @@ public class UserController {
     @GetMapping("/sign_up")
     public ModelAndView signUp() {
         return new ModelAndView("sign-up");
+    }
+
+    @PostMapping("/sign_up")
+    public ModelAndView sign_up(@Valid RegisterNewUser registerNewUser,
+                                BindingResult bindingResult) {
+        System.out.println();
+        if (bindingResult.hasErrors() || !registerNewUser.getPassword()
+                .equals(registerNewUser.getConfirmPassword())) {
+            return new ModelAndView("sign-up");
+        }
+        return new ModelAndView();
+    }
+
+    @RequestMapping("/login-error")
+    public ModelAndView errorLogin() {
+        ModelAndView modelAndView = new ModelAndView("sign-in");
+        modelAndView.addObject("bad_credentials", true);
+        System.out.println("ERROR LOGIN");
+        return modelAndView;
     }
 }
