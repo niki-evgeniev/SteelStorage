a1069735f14cba31f1ceda141bcaa91d20422107
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-03-30 05:10:12 +0300

    Add err DTO

---
+++ src/main/java/nevg/steelstorage/Models/DTO/ErrorRegisterNewUser.java
@@ -0,0 +1,17 @@
+package nevg.steelstorage.Models.DTO;
+
+public class ErrorRegisterNewUser {
+
+    private String error;
+
+    public ErrorRegisterNewUser() {
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
