613bb98f9f50406b484aa85014ae0ec974b8f333
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-04-06 02:54:51 +0300

    Change lang for navbar

---
+++ src/main/java/nevg/steelstorage/Controller/SteelController.java
@@ -0,0 +1,32 @@
+package nevg.steelstorage.Controller;
+
+
+import nevg.steelstorage.Models.DTO.Steel.GetDiameterDTO;
+import nevg.steelstorage.Service.SteelService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class SteelController {
+    private final SteelService steelService;
+
+    public SteelController(SteelService steelService) {
+        this.steelService = steelService;
+    }
+
+    @GetMapping("/earnings")
+    public ModelAndView earnings() {
+        return new ModelAndView("earnings");
+    }
+
+    @GetMapping("/add_steel")
+    public ModelAndView addSteel() {
+        List<GetDiameterDTO> getDiameterDTOS =  steelService.getDiameterForAllSteel();
+        ModelAndView modelAndView = new ModelAndView("addSteel");
+        modelAndView.addObject("diameters",getDiameterDTOS );
+        return modelAndView;
+    }
+}
---
+++ src/main/java/nevg/steelstorage/Models/DTO/Steel/GetDiameterDTO.java
@@ -0,0 +1,17 @@
+package nevg.steelstorage.Models.DTO.Steel;
+
+public class GetDiameterDTO {
+
+    private String steelSize;
+
+    public GetDiameterDTO() {
+    }
+
+    public String getSteelSize() {
+        return steelSize;
+    }
+
+    public void setSteelSize(String steelSize) {
+        this.steelSize = steelSize;
+    }
+}
