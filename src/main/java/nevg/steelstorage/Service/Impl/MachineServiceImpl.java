5d26f1293f2bcb78a8fa374dc8ad303c27489173
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-04-07 00:42:15 +0300

    Added implementations for adding new machine

--- src/main/java/nevg/steelstorage/Service/Impl/MachineServiceImpl.java
+++ src/main/java/nevg/steelstorage/Service/Impl/MachineServiceImpl.java
@@ -2,7 +2,9 @@ package nevg.steelstorage.Service.Impl;
 
 import nevg.steelstorage.Models.DTO.machine.AddMachineDTO;
 import nevg.steelstorage.Models.Entity.Machine;
+import nevg.steelstorage.Models.Entity.User;
 import nevg.steelstorage.Repository.MachineRepository;
+import nevg.steelstorage.Repository.UserRepository;
 import nevg.steelstorage.Service.MachineService;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,10 +16,13 @@ import java.util.Optional;
 public class MachineServiceImpl implements MachineService {
 
     private final MachineRepository machineRepository;
+    private final UserRepository userRepository;
     private final ModelMapper modelMapper;
 
-    public MachineServiceImpl(MachineRepository machineRepository, ModelMapper modelMapper) {
+    public MachineServiceImpl(MachineRepository machineRepository, UserRepository userRepository,
+                              ModelMapper modelMapper) {
         this.machineRepository = machineRepository;
+        this.userRepository = userRepository;
         this.modelMapper = modelMapper;
     }
 
@@ -25,13 +30,18 @@ public class MachineServiceImpl implements MachineService {
     public boolean addNewMachine(AddMachineDTO addMachineDTO, UserDetails userDetails) {
 
         Optional<Machine> bySerialNumber = machineRepository.findBySerialNumber(addMachineDTO.getSerialNumber());
-        if (bySerialNumber.isPresent()){
+        if (bySerialNumber.isPresent()) {
             return false;
         }
 
         Machine newMachine = new Machine();
         newMachine = modelMapper.map(addMachineDTO, Machine.class);
-//        machineRepository.save(newMachine);
+        String username = userDetails.getUsername();
+        Optional<User> findUserAddMachine = userRepository.findByEmail(userDetails.getUsername());
+        if (findUserAddMachine.isPresent()) {
+            newMachine.setMachineList(findUserAddMachine.get());
+            machineRepository.save(newMachine);
+        }
         return true;
     }
 }
