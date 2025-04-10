95be5a4bb2ec6b6c359baaf76164f9d68660a4e4
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-04-11 00:39:41 +0300

    Update EarningsController.java

--- src/main/java/nevg/steelstorage/Controller/EarningsController.java
+++ src/main/java/nevg/steelstorage/Controller/EarningsController.java
@@ -13,10 +13,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class EarningsController {
